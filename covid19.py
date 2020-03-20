72d0a74a3dca863f0786f6b8245fadd4c90db249
apkash <primeashwini992001@gmail.com>
2020-03-20 15:56:32 +0530

    Add files via upload

---
+++ covid19.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from time import sleep
+import datetime
+from urllib import request
+
+url="https://www.worldometers.info/coronavirus/"
+
+logo = '''
+╔═╗┌─┐┬─┐┌─┐┌┐┌┌─┐┬  ┬┬┬─┐┬ ┬┌─┐
+║  │ │├┬┘│ ││││├─┤└┐┌┘│├┬┘│ │└─┐
+╚═╝└─┘┴└─└─┘┘└┘┴ ┴ └┘ ┴┴└─└─┘└─┘
+╔╦╗┬─┐┌─┐┌─┐┬┌─┌─┐┬─┐
+ ║ ├┬┘├─┤│  ├┴┐├┤ ├┬┘
+ ╩ ┴└─┴ ┴└─┘┴ ┴└─┘┴└─
+'''
+print(logo)
+
+time = datetime.datetime.now().strftime('%d %B %Y @ %H:%M')
+
+print("Checked on: ",time)
+# Original Code
+
+response = requests.get(url)
+
+data = BeautifulSoup(response.text, "html.parser")
+
+gcases=data.find(id="maincounter-wrap")
+print(gcases.get_text())
+
+gdeaths = gcases.find_next_sibling('div', id="maincounter-wrap")
+print(gdeaths.get_text())
+
+grecovered = gdeaths.find_next_sibling('div', id="maincounter-wrap")
+print(grecovered.get_text())
+
+# Till here
+
+web = request.urlopen(url).read().decode('utf8')
+soup = BeautifulSoup(web, 'html.parser')
+
+
+table = soup.find("table")
+body = table.find('tbody')
+rows = body.find_all('tr')
+
+print("\n\n")
+
+print("Country\tTotal Cases\tNew Cases\tTotal Deaths\tNew Deaths\tRecovered\tActive Case\tCritical\tCases/Million POP\n")
+
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text for ele in cols]
+    for col in cols:
+        if col=="" or col==None:
+            col=0
+        print(str(col), end="\t\t")
+    sleep(0.25)
+    print("\n")
+
+
